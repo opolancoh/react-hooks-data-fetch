8e02c834a3f6635b8a5a44a9418b7bc7328038b2
Oscar Polanco <opolancoh@gmail.com>
2019-10-30 21:20:23 +0000

    v1.0 data fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
+import http from 'axios';
 
 function App() {
-  return <h1>Hello World!</h1>;
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState('redux');
+  const [search, setSearch] = useState('redux');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const response = await http(`https://hn.algolia.com/api/v1/search?query=${search}`);
+        setData(response.data);
+      } catch (error) {
+        setIsError(true);
+      }
+      setIsLoading(false);
+    }
+    fetchData();
+  }, [search]);
+
+  return (
+    <Fragment>
+      <input type="text" value={query} onChange={event => setQuery(event.target.value)} />
+      <button type="button" onClick={() => setSearch(query)}>
+        Search
+      </button>
+
+      {isError && <div>Something went wrong ...</div>}
+
+      {isLoading ? (
+        <div>loading ...</div>
+      ) : (
+        <ul>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </Fragment>
+  );
 }
 
 export default App;
